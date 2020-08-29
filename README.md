dfecbb2160a79a8e9de5974bf7ca3a6e00e0561d
Ramtin Soltani <ramtinsoltani93@gmail.com>
2020-08-29 05:11:15 -0700

    Added @steroids/core as a dependency from Github and changed the postinstall script to build @steroids/core after install; updated the README; added unit tests.

--- README.md
+++ README.md
@@ -4,12 +4,8 @@ This package contains pipes for Singular framework, installed from `npm install 
 
 # Setup
 
-  1. Clone [repo](https://github.com/singularframework/core) from the appropriate branch
-  2. Inside repo run `npm start` and then `sudo npm link` to create a global symlink
-  3. Clone [repo](https://github.com/singularframework/validators) from the appropriate branch
-  4. Inside repo run `npm start` and then `sudo npm link` to create a global symlink
-  5. Install dependencies inside `@singular/pipes` repo: `npm install`
-  6. Whenever a dependency is installed, the symlink needs to be recreated: `npm link @singular/core`
+  1. Clone this repo
+  2. Run `npm install` to install all dependencies
 
 # NPM Scripts
 
--- test/src/main.spec.ts
+++ test/src/main.spec.ts
@@ -1,9 +1,46 @@
 import 'source-map-support/register';
+import { expect } from 'chai';
 import { pipe } from '../..';
-import { $ } from '@singular/validators';
+import { ValidatorFunction } from '@singular/core';
 
 describe('Pipes', function() {
 
+  it('should pipe values conditionally correctly', async function() {
 
+    const body1 = {
+      operation: 'addition',
+      value1: 2,
+      value2: 1,
+      value3: undefined
+    };
+
+    const body2 = {
+      operation: 'subtraction',
+      value1: 2,
+      value2: 1,
+      value3: undefined
+    };
+
+    const operationIs = (operation: string): ValidatorFunction => {
+
+      return (value: any, rawValues?: any) => {
+
+        return rawValues?.operation === operation;
+
+      };
+
+    }
+
+    let pipes = pipe.setRef('value1').incrementRef('value2').when(operationIs('addition')).__exec();
+
+    expect(await pipes(body1.value3, body1)).to.equal(3);
+    expect(await pipes(body2.value3, body2)).to.be.undefined;
+
+    pipes = pipe.setRef('value1').decrementRef('value2').when(operationIs('subtraction')).__exec();
+
+    expect(await pipes(body1.value3, body1)).to.be.undefined;
+    expect(await pipes(body2.value3, body2)).to.equal(1);
+
+  });
 
 });
